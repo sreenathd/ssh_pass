8a00408ff565cb871959539bb03abb2cdf47ac14
Sreenath D <sreenathd2002@gmail.com>
2020-09-24 12:03:24 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,4 @@
+const execSync = require('child_process').execSync;
+// import { execSync } from 'child_process';  // replace ^ if using ES modules
+const output = execSync('ls', { encoding: 'utf-8' });  // the default is 'buffer'
+console.log('Output was:\n', output);
